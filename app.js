fb6e72dc026af6e930186cd1de65d59f9577486b
Zach Alcerro <zachalcerro@gmail.com>
2019-07-21 13:57:26 +0900

    changes click event to use button target

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const missed = 0;
-const startGame = document.getElementsByClassName('btn_reset');
+const startGame = document.querySelector('.btn__reset');
 const startScreen = document.getElementById('overlay'); 
 
-startScreen.addEventListener('click', (e) => {  
+startGame.addEventListener('click', (e) => {  
     startScreen.style.visibility = 'hidden';
 });
 
