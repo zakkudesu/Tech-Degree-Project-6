743aa47c160af48af1283c9cb56beb26f47feaee
Zach Alcerro <zachalcerro@gmail.com>
2019-07-21 22:38:29 +0900

    begins adding phrase to display function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const phrase = document.getElementById('phrase');
 const missed = 0;
 const startGame = document.querySelector('.btn__reset');
 const startScreen = document.getElementById('overlay'); 
+const gameDisplay = document.querySelector('#phrase ul');
 
 startGame.addEventListener('click', (e) => {  
     startScreen.style.visibility = 'hidden';
@@ -20,8 +21,22 @@ function getRandomPhraseAsArray(arr){
     //do stuff to any arr that is passed in
     let pickPhrase = arr[Math.floor(Math.random()*arr.length)];
     let splitPhrase = pickPhrase.split(' ');
-    console.log(splitPhrase);
+    
+    // console.log(splitPhrase);
     return splitPhrase;
  } 
 
-getRandomPhraseAsArray(gamePhrases);
+// getRandomPhraseAsArray(gamePhrases);
+
+function addPhraseToDisplay(splitArr){
+    // do stuff any arr that is passed in, and add to `#phrase ul`
+    
+    for (let i = 0; i < splitArr.length; i++){
+        const li = document.createElement('li');
+        
+
+        gameDisplay.appendChild();   
+    }
+}
+
+addPhraseToDisplay();
